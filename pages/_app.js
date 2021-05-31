f6b9b8bc98fc6c1dfb3f7dec1416e68bdc042472
kwonjunhyuk <kwonh11@caroom.co.kr>
2021-05-31 17:03:17 +0900

    Feat: eslint, prettier 셋팅

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
-import Link from 'next/link';
+import Link from "next/link";
 import styled, { createGlobalStyle, css } from "styled-components";
 import reset from "styled-reset";
+
 const Container = styled.div`
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 `;
 const GlobalStyle = createGlobalStyle`
   ${reset};
@@ -12,23 +13,23 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 const GNB = styled.div`
-    width: '100%';
-    height: 50px;
-    position: fixed;
+  width: "100%";
+  height: 50px;
+  position: fixed;
 `;
-export default function App ({ Component, pageProps }) {
-    return (
-        <Container>
-            <GlobalStyle />
-            <GNB>
-            <Link href="/page1">
-                <a>page1</a>
-            </Link>
-            <Link href="/page2">
-                <a>page2</a>
-            </Link>
-            </GNB>
-            <Component {...pageProps} />
-        </Container>
-    );
+export default function App({ Component, pageProps }) {
+  return (
+    <Container>
+      <GlobalStyle />
+      <GNB>
+        <Link href="/page1">
+          <a>page1</a>
+        </Link>
+        <Link href="/page2">
+          <a>page2</a>
+        </Link>
+      </GNB>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
